0f4549c6f8996e84efa737764bfe8d12bfcd5bf4
DaveanandMannie <daveanandmannie@gmail.com>
2024-07-01 19:34:39 -0400

    small refactor for clarity

--- e2e_test.py
+++ e2e_test.py
@@ -1,40 +1,49 @@
-import first_iter
+from first_iter import parse_odoo_pdf, left_join, generate_label, generate_label_data
 import functools
 import csv
 import tracemalloc
 
-odoo_pdf: str = 'tests/Picking Operations - S&S Canada - WH_IN_00129.pdf'
+odoo_pdf: str = 'test_targets/Picking Operations - S&S Canada - WH_IN_00129.pdf'
 product_csv: str = 'documents/product_code_case.csv'
-csv_output: str = 'tests/csv_history'
-label_output: str = 'tests/hotfolder'
+csv_output: str = 'test_targets/csv_history'
+label_output: str = 'test_targets/hotfolder'
 
 
-def mem_perf(test_func):
+def print_memory_usage(test_func):
     @functools.wraps(test_func)
     def wrapper(*args, **kwargs):
         tracemalloc.start()
         result = test_func(*args, **kwargs)
         current, peak = tracemalloc.get_traced_memory()
         tracemalloc.stop()
-        print(
-            f'Memory usage (Mb)\n===============\nCurrent:\n    {current / 10 ** 6}\nPeak:\n    {peak / 10 ** 6}'
+        std_out: str = (
+            f'Memory Usage : {str(test_func)}\n'
+            f'=====================================================================\n'
+            f'Current:\n    {current / 10 ** 6}MB\n'
+            f'Peak:\n    {peak / 10 ** 6}MB'
         )
+        print(std_out)
         return result
 
     return wrapper
 
 
-@mem_perf
-def generic_test(receiving_file: str, product_var_csv: str, label_output_dir: str, csv_output_dir: str):
-    data: list = first_iter.parse_odoo_pdf(receiving_file)
-    receiving_data: str = first_iter.left_join(data, product_var_csv, csv_output_dir, receiving_file)
+@print_memory_usage
+def full_test(receiving_file: str, product_var_csv: str, label_output_dir: str, csv_output_dir: str):
+    data: list = parse_odoo_pdf(receiving_file)
+    receiving_data: str = left_join(data, product_var_csv, csv_output_dir, receiving_file)
     with open(receiving_data) as file:
         reader: csv.reader = csv.reader(file)
         next(reader)  # skipping headers
         for line in reader:
-            label_data: dict = first_iter.generate_label_data(line, receiving_file)
-            first_iter.generate_label(label_output_dir, label_data)
+            label_data: dict = generate_label_data(line, receiving_file)
+            generate_label(label_output_dir, label_data)
     return
 
 
-generic_test(odoo_pdf, product_csv, label_output, csv_output)
+@print_memory_usage
+def store_label_data(receiving_file:str):
+    pass
+
+
+full_test(odoo_pdf, product_csv, label_output, csv_output)
