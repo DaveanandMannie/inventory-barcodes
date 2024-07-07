62b5360d3c8b88bdf304cc1b3e5f5374cb1a6eb7
DaveanandMannie <daveanandmannie@gmail.com>
2024-07-06 21:46:41 -0400

    UI can generate label data list

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
+import csv
 from customtkinter import CTkButton, filedialog, CTkLabel, StringVar, CTkFrame, CTk
 from odoogen import *
 from e2e_test import print_memory_usage
 import json
+from typing import Callable
 
 with open('./documents/test_config.json') as file:
     config: dict = json.load(file)
@@ -14,6 +16,7 @@ class PDFFrame(CTkFrame):
         self.grid_columnconfigure(index=1, weight=2)
         self.default_pdf: str = 'No file selected'
         self.selected_file = StringVar(value=self.default_pdf)
+        self.callbacks: list = []  # depending on how I control data flow I might back this a static function
 
         self.file_frame_label = CTkLabel(self, text='"Picking Operations" PDF:', font=('consolas', 19, 'bold'))
         self.file_frame_label.grid(row=0, column=0, sticky='nsew', padx=20)
@@ -31,10 +34,25 @@ class PDFFrame(CTkFrame):
         file_path: str = filedialog.askopenfilename(initialdir=self.master.default_pdf_dir.get())
         if file_path:
             self.selected_file.set(file_path)
+            self._execute_callbacks(file_path)
         return
 
+    def register_callback(self, callback: Callable):
+        """Register call back to be executed when a file is selected"""
+        self.callbacks.append(callback)
+        return
+
+    def _execute_callbacks(self, file_path: str):
+        """Execute all registered call backs"""
+        for callback in self.callbacks:
+            callback(file_path)
+        return
+
+    # TODO: Callback? :(
     def reset_pdf(self):
         self.selected_file.set(self.default_pdf)
+        self.master.odoo_ref.set(self.default_pdf)
+        return
 
 
 # TODO: create a config pop up with a password
@@ -71,7 +89,8 @@ class App(CTk):
         self._set_appearance_mode('dark')
         self.title('Receiving Barcode Generator')
         self.geometry("1080x720")
-        self.config = config_file
+        self.config: dict = config_file
+        self.label_data: list[dict] = []
 
         self.grid_columnconfigure(index=0, weight=1)
         self.grid_columnconfigure(index=1, weight=1)
@@ -79,6 +98,7 @@ class App(CTk):
 
         self.PDFFrame = PDFFrame(self)
         self.PDFFrame.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=3)
+        self.PDFFrame.register_callback(self.store_label_data)
 
         self.hotfolder_dir: StringVar = StringVar(value=self.config['hotfolder_dir'])
         self.default_pdf_dir: StringVar = StringVar(value=self.config['default_pdf_dir'])
@@ -86,7 +106,7 @@ class App(CTk):
         self.product_var_csv: StringVar = StringVar(value=self.config['product_var_csv'])
 
         # TODO: get from odoo-gen
-        self.odoo_ref: StringVar = StringVar(value='WH/IN/00014')
+        self.odoo_ref: StringVar = StringVar(value='No file selected')
 
         self.config_frame = ConfigFrame(self)
         self.config_frame.grid(row=1, column=2, padx=20, sticky='e')
@@ -95,6 +115,25 @@ class App(CTk):
         hotfolder_dir: str = filedialog.askdirectory(initialdir=self.hotfolder_dir.get())
         if hotfolder_dir:
             self.hotfolder_dir.set(hotfolder_dir)
+        return
+
+    def store_label_data(self, file_path):
+        data: list = parse_odoo_pdf(file_path)
+
+        receiving_data: str = left_join(
+            cleaned_data=data,
+            product_var_export=self.product_var_csv.get(),
+            output_file_path=self.joined_csv_dir.get(),
+            receiving_pdf=file_path
+        )
+        # TODO  decide weather to add this functionality to odoo-gen -> probably
+        with open(receiving_data) as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            for line in reader:
+                label_dict: dict = generate_label_data(line, file_path)
+                self.label_data.append(label_dict)
+            self.odoo_ref.set(self.label_data[0]['in_ref'])
         return
 
 
